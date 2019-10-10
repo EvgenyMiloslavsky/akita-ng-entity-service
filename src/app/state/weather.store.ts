d66ed6a56778ab7c742b4599ebafefe144f68256
Evgeny Ogurko <evgenyo@mprest.com>
2019-10-10 18:53:25 +0300

    fix

--- src/app/state/weather.store.ts
+++ src/app/state/weather.store.ts
@@ -5,7 +5,7 @@ import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
 export interface WeatherState extends EntityState<Weather> {}
 
 @Injectable({ providedIn: 'root' })
-@StoreConfig({ name: 'search' })
+@StoreConfig({ name: 'locations/v1/cities/search' })
 export class WeatherStore extends EntityStore<WeatherState> {
 
   constructor() {
